226adcfad1c77bf580d2746da71b49a142beff2b
Svenden123 <buhrus1@yandex.ru>
2021-10-04 21:29:41 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+#list1 = [1, 2, 3, 4, 5]
+#list2 = [1, 2, 3, 4, "q"]
+#list3 = ["a", None, True, []]
+#print(list1 < list2)
+#print(list1 < list3)
+#list2.append([1,2])
+#print(list2)
+#list1.insert(2,[1,2])
+#print(list1)
+#list3.clear()
+#print(list3)
+#list4 = list1.copy()
+#print(list4)
+#print(id(list4))
+
+#list1 = [1,2,3,4,5,100,100,'1']
+#print(list1.count(100))
+#print(list1.count('fgggfg'))
+#print(1 in list1)
+#list1.extend([1,2])
+#print(list1)
+#print(list1.index(4))
+
+#print(list1.pop(3))
+#print(list1)
+
+#list1.remove(3)
+#print(list1)
+
+#list1.reverse()
+#print(list1)
+list1 = [6,2,89,4,390,100]
+list1.sort()
+print(list1)
+
